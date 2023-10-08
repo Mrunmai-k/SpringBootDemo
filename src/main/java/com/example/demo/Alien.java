6b8835474e456f9294dea9c847aaa485502b28a1
mrunmai <mrunmaikudale18@gmail.com>
2023-10-08 23:16:02 +0530

    Use of @Autowired annotation in spring boot app

--- src/main/java/com/example/demo/Alien.java
+++ src/main/java/com/example/demo/Alien.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
+
+    @Autowired      //object will automatically be created by spring
+    Laptop lap;
+    
     public void code(){
-        System.out.println("I am coding...");
+        // System.out.println("I am coding...");
+        lap.compile();
     }
 }
